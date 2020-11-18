62c12155591ad00ea379cec6fe84c8e159c09872
Faizan Siddiqui <faizan@iitk.ac.in>
2020-11-18 14:55:25 +0530

    images in same window

--- showCameraData.py
+++ showCameraData.py
@@ -10,20 +10,28 @@ def showCam(filename):
     for data in dataset:
         frame = open_dataset.Frame()
         frame.ParseFromString(bytearray(data.numpy()))
-
-        for image in frame.images:
+        names = []
+        for i in range(len(frame.images)):
+            image = frame.images[i]
 
             nparr = np.frombuffer(image.image, np.uint8)
             img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
+            if i == 0:
+                img_concat = img_np
+            else:
+                img_concat = np.concatenate((img_concat, img_np), axis=0)
+            
             name = open_dataset.CameraName.Name.Name(image.name)
-            cv2.namedWindow(name, cv2.WINDOW_NORMAL)        # Create window with freedom of dimensions
-            cv2.resizeWindow(name, 400, 300)
-
-            cv2.imshow(name, img_np)
+            names.append(name)
+            
+        
+        cv2.namedWindow(', '.join(names), cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(', '.join(names), 400, 5*300)
+        cv2.imshow(', '.join(names), img_concat)
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
             break
-
+        
 if __name__ == '__main__':
     if len(sys.argv)!=2:
         print("Usage: python showCameraData.py dataset/data.tfrecord")
